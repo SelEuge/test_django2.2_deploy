d4b701a69a77aa115eb1e38429fcadd5172d1ad6
SelEuge <valeria_ddd291@hotmail.com>
2021-05-17 18:05:27 -0300

    Config credenciales base de datos

--- test_bigbox/settings/local.py
+++ test_bigbox/settings/local.py
@@ -7,10 +7,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bigbox',
-        'USER': 'postgres',
-        'PASSWORD': 'bigbox',
-        'HOST': 'localhost',
+        'NAME': 'dfqvn52r7e4821',
+        'USER': 'rmpdftsbjrpqoc',
+        'PASSWORD': 'eaa38ab3c55d39f3a2c4fe7e9b48f7ccd762f1eceea60db708129c4d2a5532d9',
+        'HOST': 'ec2-54-160-96-70.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
