43ce319be2ed01e51c21d59a923a94d8aeaf938d
SelEuge <valeria_ddd291@hotmail.com>
2021-05-19 09:41:23 -0300

    Enalace Mas actividades

--- bigbox/urls.py
+++ bigbox/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     # path('listarboxes', views.ListarBoxesView.as_view()),
-    path('box', views.ListAllBoxes.as_view(), name="box"),
-    path('box/<box_id>/', views.DetailBox.as_view()),
+    path('box', views.ListAllBoxes.as_view(), name="boxes"),
+    path('box/<box_id>/', views.DetailBox.as_view(), name="box"),
     path('box/<box_id>/activity', views.ListActivitiesByBox.as_view(), name="activities"),
     path('box/<box_id>/activity/<activity_id>/', views.ShowActivityByBox.as_view()),
     path('box/<slug>/', views.SearchBoxBySlug.as_view()),
--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_bigbox.settings')
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_bigbox.settings.local')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_bigbox.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_bigbox.settings.local')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_bigbox.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
