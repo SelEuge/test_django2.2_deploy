72d8481fa5e637e91c57c4f75e2e7ccc16107578
SelEuge <valeria_ddd291@hotmail.com>
2021-06-26 15:45:01 -0300

    Polls/Tutorial03

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,58 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
+"""def index(request):
+    return HttpResponse('Hello, World. You are at the polls app')"""
+
+"""def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)"""
+
+"""def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))"""
+
+
+# Un atajo: render()
 def index(request):
-    return HttpResponse('Hello, World. You are at the polls app')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
+# --------------------Vistas details---------------------------------------
+"""def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)"""
+
+# Generando un error 404
+"""def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})"""
+
+
+# Un atajo: get_object_or_404()
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
+# --------------------Vistas results--------------------------------------
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 
+# --------------------Vistas vote--------------------------------------
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
