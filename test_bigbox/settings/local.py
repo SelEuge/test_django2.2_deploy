f13e9bbe0f7559e693bb053dfd2353cd19337cdc
SelEuge <valeria_ddd291@hotmail.com>
2021-06-28 17:52:50 -0300

    Migraciones-Parte02-03

--- test_bigbox/settings/local.py
+++ test_bigbox/settings/local.py
@@ -7,10 +7,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dfqvn52r7e4821',
-        'USER': 'rmpdftsbjrpqoc',
-        'PASSWORD': 'eaa38ab3c55d39f3a2c4fe7e9b48f7ccd762f1eceea60db708129c4d2a5532d9',
-        'HOST': 'ec2-54-160-96-70.compute-1.amazonaws.com',
+        'NAME': 'bigbox',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
         'PORT': '5432',
         }
     }
