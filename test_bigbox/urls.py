df547ba79257f376fde65148b03a3d302f2d29b4
SelEuge <valeria_ddd291@hotmail.com>
2021-06-29 17:30:43 -0300

    Archivos estaticos2

--- test_bigbox/urls.py
+++ test_bigbox/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   re_path('', include('bigbox.urls')),
                   path('__debug__/', include(debug_toolbar.urls)),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
