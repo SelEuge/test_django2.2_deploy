5c4e767ca0e08a5c2c57f430889f99aec0fdb02c
SelEuge <valeria_ddd291@hotmail.com>
2021-07-02 16:42:01 -0300

    6 - Viewsets y Enrutadores

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
@@ -16,7 +17,7 @@ from snippets import views
 urlpatterns = format_suffix_patterns(urlpatterns)"""
 
 # API endpoints
-urlpatterns = format_suffix_patterns([
+"""urlpatterns = format_suffix_patterns([
     path('', views.api_root),
     path('snippets/',
         views.SnippetList.as_view(),
@@ -34,3 +35,11 @@ urlpatterns = format_suffix_patterns([
         views.UserDetail.as_view(),
         name='user-detail')
 ])
+"""
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
